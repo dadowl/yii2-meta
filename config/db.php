67816acf36e3acbe967d31fb1c20440068cb8bcf
DadOwl <fayence@yandex.ru>
2020-09-02 12:33:37 +0700

    add migrations

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2-meta',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_meta',
     'username' => 'root',
     'password' => 'root',
     'charset' => 'utf8',
